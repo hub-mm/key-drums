e22c1d33c123ecb36161c501ccc5c7629dd3a1c6
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-05-28 20:29:53 +0700

    Finish HTML, CSS and JS. Start Debugging.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-/// javascript
+
+function playSound(e) { 
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    
+    if (!audio) {
+    console.log('No audio element found')
+    return;
+    }
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    console.log('Transition end event fired:', e);
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+    console.log('Remove playing class from:', this);
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound)
