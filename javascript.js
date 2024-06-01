7f51ee50b6fe6aadcb91140df9def8aba6e00707
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-06-01 12:43:16 +0700

    Debug click now makes sound - problem with event handeling (e.code -> code). Add cursor: pointer in css.

--- javascript.js
+++ javascript.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     keys.forEach(key => {
         key.addEventListener('click', playSound);
-        // key.addEventListener('keydown', playSound);
         key.addEventListener('transitionend', removeTransition);
     });
 
@@ -18,40 +17,37 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Transition end and click event listeners added to keys:', keys) // Debugging log
 
     window.addEventListener('keydown', playSound);
-    // window.addEventListener('click', playSound);
 
     console.log('Keydown and click event listener added to window'); // Debugging log
 
 });
 
 function playSound(e) {
-    let keyCode;
+    let code;
 
     if (e.type === 'keydown') {
-        keyCode = e.code;
+        code = e.code;
     } else if (e.type === 'click') {
-        const keyElement = e.target.closest('.data');
-
-        keyCode = keyElement ? keyElement.dataset.key : null;
+        const keyElement = e.target.closest('.key');
+        code = keyElement ? keyElement.dataset.key : null;
     }
-
-
+    
     console.log('Event type:', e.type); // Debugging log
-    console.log('Key pressed or clicked:', keyCode);  // Debugging log
-
-    if (!keyCode) {
-        console.log('No keyCode found for event:', e); // Debugging log
+    console.log('Key pressed or clicked:', code);  // Debugging log
+    
+    const audio = document.querySelector(`audio[data-key='${code}']`);
+    const key = document.querySelector(`.key[data-key='${code}']`);
+    
+    if (!code) {
+        console.log('No code found for event:', e); // Debugging log
         return;
     }
-
-    const audio = document.querySelector(`audio[data-key='${e.code}']`);
-    const key = document.querySelector(`.key[data-key='${e.code}']`);
 
     console.log('Audio element:', audio); // Log found audio element
     console.log('Key element:', key); // Log found key element
 
     if (!audio) {
-        console.log('No audio element found for key:', keyCode); // Debugging log
+        console.log('No audio element found for key:', code); // Debugging log
         return;
     }
 
@@ -63,28 +59,21 @@ function playSound(e) {
     key.classList.add('playing');
 
     console.log('Playing class added to:', key); // Debugging log
-
 }
 
 function removeTransition(e) {
 
-    // console.log('Transition end event fired:', e); // Debugging log
+    console.log('Transition end event fired:', e); // Debugging log
 
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 
-    // console.log('Remove playing class from:', this); // Debugging log
+    console.log('Remove playing class from:', this); // Debugging log
 
 }
 
 function changeBackground(e) {
-    // let background = document.body.style.backgroundImage;
-    // let drumKit = document.getElementById('drum-kit');
-    // let concertCrowd = document.getElementById('concert-crowd');
-
-    // console.log('Drum Kit button:', drumKit);
-    // console.log('Concert Crowd button:', concertCrowd);
-
+    
     console.log('Clicked element:', e.target);
 
     if (e.target.id === 'drum-kit') {
